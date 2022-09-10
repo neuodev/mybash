3b98d840a5393c60688d2d7a97dc0d2f5e3e3ffd
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-10 07:06:10 +0200

    Add second lang example

--- src/executor.rs
+++ src/executor.rs
@@ -1,8 +1,14 @@
-use crate::{conditions::Condition, echo::Echo, lang_parser::Expression, variables::VarValue};
+use crate::{
+    cmp::CompareExpr,
+    conditions::Condition,
+    echo::Echo,
+    lang_parser::Expression,
+    variables::{VarValue, Variable},
+};
 use std::collections::HashMap;
 
 pub struct Executor<'a> {
-    vars: HashMap<&'a String, &'a VarValue>,
+    vars: HashMap<&'a str, &'a VarValue>,
     expressions: &'a Vec<Expression>,
 }
 
@@ -11,7 +17,7 @@ impl<'a> Executor<'a> {
         let mut vars = HashMap::new();
         expressions.iter().for_each(|e| {
             if let Expression::Var(var) = e {
-                vars.insert(&var.name, &var.value);
+                vars.insert(var.name.as_str(), &var.value);
             }
         });
 
@@ -21,22 +27,46 @@ impl<'a> Executor<'a> {
     pub fn execute(&self) {
         for expr in self.expressions {
             if let Expression::Echo(Echo(s)) = expr {
-                let value = match self.vars.get(&s) {
-                    Some(val) => match val {
-                        VarValue::Int(val) => val.to_string(),
-                        VarValue::Str(val) => val.to_string(),
-                    },
-                    None => s.to_string(),
-                };
-
-                println!("{}", value);
+                self.eval_echo(s);
             } else if let Expression::Condition(con) = expr {
-                let Condition {
-                    if_expr,
-                    else_expr,
-                    condition,
-                } = con.as_ref();
+                self.eval_condition(con.as_ref())
             }
         }
+    }
+
+    fn eval_echo(&self, s: &str) {
+        match self.vars.get(&s) {
+            Some(val) => println!("{}", val),
+            None => println!("{}", s),
+        };
+    }
+
+    fn eval_condition(&self, con: &Condition) {
+        let Condition {
+            if_expr,
+            else_expr,
+            condition,
+        } = con;
+        let CompareExpr {
+            left,
+            right,
+            operator,
+        } = condition;
+
+        let left_val = self.found_var_or_create(left);
+        let right_val = self.found_var_or_create(right);
+    }
+
+    fn found_var_or_create(&self, s: &str) -> VarValue {
+        if let Some(v) = self.vars.get(s) {
+            return (*v).clone();
+        }
+
+        let var = match s.parse::<i32>() {
+            Ok(num) => VarValue::Int(num),
+            Err(_) => VarValue::Str(s.to_string()),
+        };
+
+        var
     }
 }
