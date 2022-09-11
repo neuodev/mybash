2062185686dd5ff2941f216182727dfe68332601
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-11 06:16:13 +0200

    Support strings with single or double or without quotes

--- src/regex.rs
+++ src/regex.rs
@@ -5,6 +5,6 @@ pub const RE_VAR: &str =
 pub const RE_ECHO: &str = r#"(?m)echo\s+('|")?(?P<expr>[^'"\n]+)('|")?"#;
 /// A regular expression to match `if else` statments. [Interactive example](https://regex101.com/r/cfub08/1)
 pub const RE_IF_ELSE: &str = r#"(?m)if\s+(?P<con>[^\n]+)\ndo\s+(?P<if_expr>[^\n]+)(\nelse\s*\ndo\s+(?P<else_expr>[^\n]+))?\nendif"#;
-
-/// A regular expression to evaluate comparison operations. [Interactive example](https://regex101.com/r/QLgk8r/1)
-pub const RE_CMP: &str = r#"(?m)(?P<left>[^\s]+)\s+(?P<op>[^\s]+)\s+(?P<right>.+)"#;
+/// A regular expression to evaluate comparison operations. [Interactive example](https://regex101.com/r/OjbUgO/1)
+pub const RE_CMP: &str =
+    r#"(?m)('|")?(?P<left>[^\s'"\n]+)('|")?\s+(?P<op>[^\s]+)\s+('|")?(?P<right>[^'"\n]+)('|")?"#;
